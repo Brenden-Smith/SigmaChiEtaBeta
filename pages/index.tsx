a06b07e72cee8c917eb2cd2e1d9e76432e52d24e
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-04-26 21:11:42 -0700

    Update index.tsx
    - Implement analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,30 +10,42 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import { ThemeProvider } from '@emotion/react'
 import IosShareSharpIcon from "@mui/icons-material/IosShareSharp";
 import { doc, onSnapshot } from 'firebase/firestore'
-import { db } from '../firebase'
+import { db, useAnalytics } from '../firebase'
+import { Analytics, logEvent } from "firebase/analytics";
 
 const theme = createTheme({
   typography: {
     fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
+      "rift-soft",
+      "geller-headline"
     ].join(","),
+    button: {
+      fontFamily: "geller-headline",
+      fontWeight: 300,
+      fontSize: "1.2rem",
+      textTransform: "none",
+    }
   },
   palette: {
     primary: {
       main: "#00A3E0",
-
+    },
+    secondary: {
+      main: "#FEC141",
     }
-  }
+  },
 });
+
+/**
+ * Helper function to log events to analytics
+ */
+function logAnalyticsEvent(analytics: Analytics, title: string, url: string) {
+  if (analytics)
+    logEvent(analytics, "link", {
+      title: title,
+      url: url,
+    });
+}
 
 /**
  * Profile photo component
@@ -48,9 +60,11 @@ const ProfilePhoto = () => {
   );
 }
 
-const Links = ({items}: {
+const Links = ({items, analytics}: {
   items: any,
+  analytics: Analytics
 }) => {
+
   return <Stack direction="column" spacing={2}>
     {items.map((item: any, index: any) => {
       return (
@@ -65,10 +79,9 @@ const Links = ({items}: {
           }}
           href={item.url}
           target="_blank"
+          onClick={() => logAnalyticsEvent(analytics, item.title, item.url)}
         >
-          <Typography>
-            {item.title}
-          </Typography>
+          {item.title}
         </Button>
       );
     })}
@@ -76,20 +89,39 @@ const Links = ({items}: {
 }
 
 
-const SocialLinks = ({items}: {
-  items: any
+const SocialLinks = ({items, analytics}: {
+  items: any,
+  analytics: Analytics,
 }) => {
   const icon = {height: 35, width: 35};
 
   return (
     <Stack direction="row">
-      <IconButton size="large" href={items.instagram} target="_blank">
+      <IconButton
+        size="large"
+        href={items.instagram}
+        target="_blank"
+        color="secondary"
+        onClick={() => logAnalyticsEvent(analytics, "Instagram", items.instagram)}
+      >
         <InstagramIcon fontSize="large" />
       </IconButton>
-      <IconButton size="large" href={items.facebook} target="_blank">
+      <IconButton
+        size="large"
+        href={items.facebook}
+        target="_blank"
+        color="secondary"
+        onClick={() => logAnalyticsEvent(analytics, "Facebook", items.facebook)}
+      >
         <FacebookIcon fontSize="large" />
       </IconButton>
-      <IconButton size="large" href={items.twitter} target="_blank">
+      <IconButton
+        size="large"
+        href={items.twitter}
+        target="_blank"
+        color="secondary"
+        onClick={() => logAnalyticsEvent(analytics, "Twitter", items.twitter)}
+      >
         <TwitterIcon fontSize="large" />
       </IconButton>
     </Stack>
@@ -133,6 +165,8 @@ const Home: NextPage = () => {
   const [links, setLinks] = useState<any>([]);
   const [socials, setSocials] = useState<any>([]);
 
+  const analytics = useAnalytics();
+
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(db, "links/index"), (snapshot) => {
       setLinks(snapshot.data()!.links);
@@ -143,21 +177,27 @@ const Home: NextPage = () => {
     };
   }, []);
 
-  return (
+  return <>
+    <Head>
+      <title>Sigma Chi Long Beach</title>
+      <link rel="stylesheet" href="https://use.typekit.net/day6akr.css"></link>    </Head>
     <ThemeProvider theme={theme}>
-      <div className={styles.main}>
-        <Stack direction="column" spacing={2}>
-          <Stack direction="column" className={styles.container} spacing={1}>
-            <ProfilePhoto />
-            <Typography variant="h5">Sigma Chi Long Beach</Typography>
-            <SocialLinks items={socials}/>
+      {analytics ? 
+        <div className={styles.main}>
+          <Stack direction="column" spacing={2}>
+            <Stack direction="column" className={styles.container} spacing={1}>
+              <ProfilePhoto />
+              <Typography variant="h4" sx={{fontWeight: "bold"}}>Sigma Chi Long Beach</Typography>
+              <SocialLinks items={socials} analytics={analytics}/>
+            </Stack>
+            <Links items={links} analytics={analytics}/>
           </Stack>
-          <Links items={links}/>
-        </Stack>
-        <ShareIcon/>
-      </div>
+          <ShareIcon/>
+        </div>
+        : null
+      }
     </ThemeProvider>
-  );
+  </>
 }
 
 export default Home
